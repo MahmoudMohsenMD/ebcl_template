f1b8bf3134500ae022af8c92834a89f667c10a78
Mahmoud Mohsen <142505184+MahmoudMohsenMD@users.noreply.github.com>
2024-12-05 12:26:06 +0200

    Initial commit

---
+++ images/arm64/appdev/rdb2/crinit/config_root.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Ensure init is executable
+chmod +x ./sbin/*
+
+# Ensure modprobe.sh is executable
+chmod +x ./usr/sbin/load_modules.sh
+chmod +x ./usr/sbin/ntp_time.sh
+
+# Create a fake machine-id
+echo "04711" > ./etc/machine-id
+
+# Create a hostname file
+echo "appdevrdb2" > ./etc/hostname
+
+# Create /etc/hosts
+cat > ./etc/hosts <<- EOF
+127.0.0.1       localhost
+::1             localhost ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+EOF
+
+# Enable DNS
+rm -f ./etc/resolv.conf || true
+ln -sf /var/run/resolv.conf.netifd ./etc/resolv.conf
